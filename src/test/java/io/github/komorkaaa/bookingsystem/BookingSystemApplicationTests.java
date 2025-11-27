fb81736afae9e201edb853016fab6f71f6a7101b
komorkaaa <mr.komorka@gmail.com>
2025-11-27 20:07:38 +0300

    Initial commit

---
+++ src/test/java/io/github/komorkaaa/bookingsystem/BookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.komorkaaa.bookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
